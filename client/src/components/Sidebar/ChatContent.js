b87c249177b492ea29038134aa653816d53f9a43
Jay Tran <tranquang2407@gmail.com>
2021-07-24 15:05:48 -0400

    change chatContent.js for display number of unread messages

--- client/src/components/Sidebar/ChatContent.js
+++ client/src/components/Sidebar/ChatContent.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Box, Typography } from "@material-ui/core";
+import React, { useEffect, useState } from "react";
+import { Box, Typography, Badge } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
@@ -18,14 +18,45 @@ const useStyles = makeStyles((theme) => ({
     color: "#9CADC8",
     letterSpacing: -0.17,
   },
+  unreadMessageBox: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100%",
+    margin: "auto 0",
+  },
+  unreadBadge: {
+    height: "20px",
+    width: "20px",
+    borderRadius: "50%",
+    top: "10px",
+    right: "20px",
+  },
 }));
 
+const countUnReadMessage = (messages, otherUserId) =>
+  messages.reduce(
+    (acc, message) =>
+      acc +
+      (message.readStatus === false && message.senderId === otherUserId
+        ? 1
+        : 0),
+    0
+  );
+
 const ChatContent = (props) => {
+  const [unReadMessage, setUnReadMessage] = useState(0);
+
   const classes = useStyles();
 
   const { conversation } = props;
   const { latestMessageText, otherUser } = conversation;
 
+  useEffect(() => {
+    setUnReadMessage(
+      countUnReadMessage(conversation.messages, conversation.otherUser.id)
+    );
+  }, [conversation]);
   return (
     <Box className={classes.root}>
       <Box>
@@ -35,6 +66,14 @@ const ChatContent = (props) => {
         <Typography className={classes.previewText}>
           {latestMessageText}
         </Typography>
+      </Box>
+      <Box className={classes.unreadMessageBox}>
+        <Badge
+          className={classes.unreadBadge}
+          badgeContent={unReadMessage}
+          color="primary"
+          max={99}
+        />
       </Box>
     </Box>
   );
